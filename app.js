5e0ce02fbaf35b8497a4280d635c9be54662c805
Sathish G <sathish.gajendran@gmail.com>
2016-08-02 17:57:58 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
+  var newVariable // this will FAIL travis ci lint because ';' is missing.
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis!\n') // this will FAIL travis ci lint
+  res.end('Hello Travis!\n');
 }).listen(9999, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:9999/');
